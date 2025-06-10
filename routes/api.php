90149d5f121f597ac1558ece2d61806c5b9b1135
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-10 17:02:56 +0600

    add

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ContentController;
+use App\Http\Controllers\GenreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
