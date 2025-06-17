f4ad265e5feecfbdf357fe8958befd5609b329fc
Zahidul Islam <zahidulnahid7464@gmail.com>
2025-06-17 14:52:01 +0600

    up

--- routes/api.php
+++ routes/api.php
@@ -50,7 +50,7 @@ Route::middleware('auth:api')->group(function () {
 
     // Route::middleware(['role:admin'])->group(function () {
 
-        Route::apiResource('contents', ContentController::class);
+        // Route::apiResource('contents', ContentController::class);
         Route::apiResource('genres', GenreController::class);
         Route::apiResource('subscriptions', SubscriptionController::class);
 
@@ -73,3 +73,6 @@ Route::middleware('auth:api')->group(function () {
         Route::post('/checkout', [StripePaymentController::class, 'PaymentIntent']);
     });
 });
+
+        Route::apiResource('contents', ContentController::class);
+
