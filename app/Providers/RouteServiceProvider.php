611e848123514ba9910a8c0777142597dab26213
zabeer001 <binzabirtareq@gmail.com>
2025-06-19 15:58:58 +0600

    e

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,6 +33,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
+            Route::middleware('api')
+                ->prefix('api') // Optional: if you want a common prefix
+                ->group(base_path('routes/zabeer.php'));
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
