d7fb0a49f9c59c05372f981d12d3216214d4205b
Zahidul Islam <zahidulnahid7464@gmail.com>
2025-07-06 17:45:42 +0600

    up

--- routes/api.php
+++ routes/api.php
@@ -80,7 +80,7 @@ Route::middleware('auth:api')->group(function () {
         // Route::put('update-password', [SettingController::class,'storeOrUpdateForUser']);
         // Route::get('contents/{id}', [ContentController::class,'show']);
 
-        Route::get('historys', [ContentController::class, 'historys']);
+        Route::get('historys', [ContentController::class, 'History']);
         Route::apiResource('wishlist', WishListController::class);
         Route::post('/checkout', [StripePaymentController::class, 'PaymentIntent']);
 
