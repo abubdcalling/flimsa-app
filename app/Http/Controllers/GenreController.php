2904712208a45def43343f5d22ac6a48a1045933
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-07-31 13:31:40 +0600

    add

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -301,8 +301,8 @@ class GenreController extends Controller
 
             // Upcoming contents
             $upcomingContents = Content::with('genres:id,name')
-                ->where('publish', 'public')
-                ->whereNotNull('schedule')  
+                ->where('publish', 'schedule')
+                // ->where('schedule')  
                 // ->where('schedule', '>', Carbon::now())
                 ->select('id', 'title', 'description', 'director_name', 'profile_pic', 'image', 'video1', 'publish', 'schedule', 'view_count', 'genre_id', 'created_at')
                 ->orderBy('schedule', 'asc')
