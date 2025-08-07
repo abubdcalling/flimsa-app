ac21e79fbb3072783fbbed1eee8e9b4892640290
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-08-07 14:10:07 +0600

    add

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -78,7 +78,7 @@ class StripePaymentController extends Controller
                 'quantity' => 1,
             ]],
             'mode' => 'payment',
-            'success_url' => config('app.frontend_url') . '/success?plan_type=' . $request->plan_type,
+            'success_url' => config('app.frontend_url') . '/success?plan_type=' . $request->plan_type .'session_id={CHECKOUT_SESSION_ID}',
             'cancel_url' => config('app.frontend_url') . '/cancel',
         ]);
 
